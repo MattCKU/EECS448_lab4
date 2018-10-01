05d20f10277a3544f558113189776ace7e14c427
Matt Cherry <m495c301@ku.edu>
2018-10-01 14:35:26 -0500

    Finished Exercise 3

--- Exercise3/customerBackend.php
+++ Exercise3/customerBackend.php
@@ -1,16 +1,42 @@
-<?php
+<html>
 
+<body>
+
+    <link rel="stylesheet" href="style.css">
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+
+echo "<h1> <u>Receipt</u></h1>";
 $item1 = $_POST["item1"];
 $item2 = $_POST["item2"];
 $item3 = $_POST["item3"];
-$total = ($item1 * .50) + ($item) + ($item3 * 3);
-//table formatting
-echo "<table>";
+
+$shipping = $_POST["shipping"];
+$shippingCost = 0;
+if ($shipping == "Seven Day")
+{
+    $shippingCost = 0;
+}
+else if($shipping == "Over Night")
+{
+    $shippingCost = 50.00;
+}
+else
+{
+    $shippingCost = 5.00;
+}
+$total = ($item1 * .50) + ($item2) + ($item3 * 3) + $shippingCost;
+echo "<table id='table'>";
 echo "<tr>". "<th></th>" . "<th>Quantity</th>" . "<th>Cost Per item</th>" . "<th>Sub Total</th>";
-echo "<tr>" . "<th>Item 1</th>" . "<td align='center'>$item1</td>" . "<td align='center'> $0.50</td>" . "<td align='center'>" . $item1 * .50 . "</td>";
-echo "<tr>" . "<th>Item 2</th>" . "<td align='center'>$item2</td>" . "<td align='center'> $1.00</td>" . "<td align='center'>" . $item2  . "</td>";
-echo "<tr>" . "<th>Item 3</th>" . "<td align='center'>$item3</td>" . "<td align='center'> $3.00</td>" . "<td align='center'>" . $item3 * 3 . "</td>";
+echo "<tr>" . "<th>Item 1</th>" . "<td align='center'>$item1</td>" . "<td align='center'> $0.50</td>" . "<td align='center'>$" . $item1 * .50 . "</td>";
+echo "<tr>" . "<th>Item 2</th>" . "<td align='center'>$item2</td>" . "<td align='center'> $1.00</td>" . "<td align='center'>$" . $item2  . "</td>";
+echo "<tr>" . "<th>Item 3</th>" . "<td align='center'>$item3</td>" . "<td align='center'> $3.00</td>" . "<td align='center'>$" . $item3 * 3 . "</td>";
+echo "<tr>" . "<th>Shipping</th>" . "<td align='center'>$shipping</td>" . "<td></td>" . "<td align='center'>$shippingCost</td>";
 echo "<tr>" . "<th>Total</th>" ."<td></td>" . "<td></td>" . "<td align = 'center'> $$total</td>";
 echo "</table>";
 
  ?>
+ </body>
+</html>
--- Exercise3/formChecker.js
+++ Exercise3/formChecker.js
@@ -1,3 +1,43 @@
 function validate()
 {
+    let success= false;
+    let username = document.getElementById('us').value;
+    let password = document.getElementById('pw').value;
+    if(username == "")
+    {
+        alert("Empty Username!");
+        return success;
+    }
+    if(username.search("@") == -1)
+    {
+        alert("Not valid Email");
+        return success;
+    }
+    if(password == "")
+    {
+        alert("Empty Password!");
+        return success;
+    }
+
+    let item1, item2, item3;
+    item1 = document.getElementById("Item 1").value;
+    item2 = document.getElementById("Item 2").value;
+    item3 = document.getElementById("Item 3").value;
+    if((item1 < 0) || (item2 < 0) || (item3 < 0))
+    {
+
+        alert("Invalid order value must be 0 or greater");
+        return success;
+    }
+
+
+    let shipping = document.getElementsByName("shipping");
+    //alert(shipping);
+    if(document.forms["myform"]["shipping"].value =="")
+    {
+        alert("Pick a shipping option.");
+        return success;
+    }
+    success = true;
+    return sucess;
 }
